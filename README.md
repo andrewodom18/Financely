9baf106dbd6c95028f7cb60e3171cfefc5a0b5e1
andrewodom18 <andrewodom0@gmail.com>
2024-03-26 19:52:10 -0500

    packages installed

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Financely-React
+# Financely
+
+## A financial tool for monitoring and tracking spending/saving trends across all connected accounts.
+
+### Repository Outline:
+<pre>
+
+<pre>
--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
-} from 'react-router-dom'
+} from 'react-router-dom';
 
-import './style.css'
-import Home from './views/home'
-import Features from './views/features'
-import Register from './views/register'
-import Contact from './views/contact'
-import Login from './views/login'
-import About from './views/about'
-import NotFound from './views/not-found'
+import './style.css';
+import Home from './views/home';
+import Features from './views/features';
+import Register from './views/register';
+import Contact from './views/contact';
+import Login from './views/login';
+import About from './views/about';
+import NotFound from './views/not-found';
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route component={Home} exact path="/" />
-        <Route component={Features} exact path="/features" />
-        <Route component={Register} exact path="/register" />
-        <Route component={Contact} exact path="/contact" />
-        <Route component={Login} exact path="/login" />
-        <Route component={About} exact path="/about" />
-        <Route component={NotFound} path="**" />
-        <Redirect to="**" />
+        <Route component={Home} exact path='/' />
+        <Route component={Features} exact path='/features' />
+        <Route component={Register} exact path='/register' />
+        <Route component={Contact} exact path='/contact' />
+        <Route component={Login} exact path='/login' />
+        <Route component={About} exact path='/about' />
+        <Route component={NotFound} path='**' />
+        <Redirect to='**' />
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
